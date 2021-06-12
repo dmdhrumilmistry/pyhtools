53e218a48a3e95c78be92d9f6cf7957f668c74e2
Dhrumil Mistry <dhrumilmistry2015@gmail.com>
2021-06-12 20:09:44 +0530

    Update dnsspoofer.py
    minor bug fix

--- dnsspoofer.py
+++ dnsspoofer.py
@@ -30,6 +30,10 @@ def forward_packets():
     # --queue-num -> jump to specfic queue number
     call('sudo iptables -I FORWARD -j NFQUEUE --queue-num 0', shell=True)
 
+    call('sudo iptables -I INPUT -j NFQUEUE --queue-num 0', shell=True)
+    call('sudo iptables -I OUTPUT -j NFQUEUE --queue-num 0', shell=True)
+    
+
 
 def reset_config():
     '''
@@ -43,35 +47,45 @@ def reset_config():
 def process_packet(packet):
     '''
     process received packet, everytime a packet is received.
-    prints the packet received in the queue.
+    prints the packet received in the queue and it changes 
+    the DNS response dest ip with your desired ip.
     '''
     scapy_pkt = scapy.IP(packet.get_payload())
-    print(scapy_pkt.show())
+
+    # Check for DNS layer in DNS Response Request(DNSRR) or 
+    # DNS Response Query (DNSRQ)
+    if scapy_pkt.haslayer(scapy.DNSRR):
+        print(scapy_pkt.show())
+
     packet.accept()
     
 
 ############################### Main ############################### 
 
-# print('[*] configuring packet receiver...')
+print('[*] configuring packet receiver...')
 
-# forward_packets()
-# print('[*] packet receiver configured successfully.\n')
+forward_packets()
+print('[*] packet receiver configured successfully.\n')
 
 print('[*] Creating Queue to start receiving packets.')
 try:
     queue = netfilterqueue.NetfilterQueue()
+    # Bind queue with queue-number 0
+    queue.bind(0, process_packet)
+    queue.run()
+
 except OSError as e:
     print('[-] Run script with root priviliges.')
     print(e)
-    exit()
+
+except KeyboardInterrupt:
+    print('\r[-] Keyboard Interrupt detected!')
+
 except Exception:
     print('[-] An Exception occurred while creating queue.\n', Exception)
-    exit()
-# Bind queue with queue-number 0
-queue.bind(0, process_packet)
-queue.run()
 
-print('[*] Restoring previous configurations.. please be patient...')
-reset_config()
+finally:
+    print('[*] Restoring previous configurations.. please be patient...')
+    reset_config()
 
-print('[-] Program stopped.')
+    print('[-] Program stopped.')
