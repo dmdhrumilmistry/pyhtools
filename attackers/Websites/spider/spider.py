e616f22f420211bb087b91b32494e04d421130db
Dhrumil Mistry <dhrumilmistry2015@gmail.com>
2021-07-07 20:25:42 +0530

    add message after mapping all links on website

--- attackers/Websites/spider/spider.py
+++ attackers/Websites/spider/spider.py
@@ -61,6 +61,8 @@ def crawl_website(target_url:str)->None:
     links = map_urls(target_url)
     for link in links:
         map_urls(link)
+    
+    print(BRIGHT_YELLOW + f'[*] Mapped all links found on {target_url}')
 
 
 # initialize colors 
