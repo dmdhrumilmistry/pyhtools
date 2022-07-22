8dcc40b1930e0d10d530277f1aef44e14f9baf98
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2022-07-23 01:54:18 +0530

    create android data harvestor

---
+++ examples/Android/data-cloner.py
@@ -0,0 +1,11 @@
+from pyhtools.attackers.Android.forensics.data_harvestor import DataHarvestor
+
+cloner = DataHarvestor(
+        dest_path=r'path', # path where cloned data will be stored on host machine
+        device_name='emulator-5554', # device name using `adb devices`
+        host='127.0.0.1', # adb server ip
+        port=5037, # adb server port
+    )
+
+# start cloning
+cloner.start()
