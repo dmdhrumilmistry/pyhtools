2e60e8270f6a02a069f0bdcaafeffd968b1432ed
Dhrumil Mistry <56185972+dmdhrumilmistry@users.noreply.github.com>
2023-05-16 16:43:23 +0530

    add option to gain admin permission on windows in ExecutableGenerator class
    update example

--- examples/EvilFiles/generatorScript.py
+++ examples/EvilFiles/generatorScript.py
@@ -1,12 +1,19 @@
-from pyhtools.evil_files.exec_generator import (Compilers, ExecutableGenerator)
-from os import getcwd
+from pyhtools.evil_files.exec_generator import (
+    Compilers, 
+    ExecutableGenerator,
+)
+from os.path import join as path_join
+
+
+file_path = path_join('.','Malwares', 'key_logger.py')
 
 exe = ExecutableGenerator(
-    file_path = r'D:\GithubRepos\pyhtools\examples\EvilFiles\Malwares\key_logger.py', # evil program file path
+    file_path = file_path, # evil program file path
     output_dir = '.', # output directory
     compiler = Compilers.DEFAULT, # compile using DEFAULT, CLANG, MINGW
     onefile = True, # creates single exe file
     remove_output = True, # deletes all compiled files and retains only exe
+    window_uac_perms=True, # asks user for admin rights on windows (only for windows machine)   
 )
 
 return_code = exe.generate_executable()
--- pyhtools/evil_files/exec_generator.py
+++ pyhtools/evil_files/exec_generator.py
@@ -18,7 +18,7 @@ class ExecutableGenerator:
     creates executable from python script.
     '''
 
-    def __init__(self, file_path: str, output_dir: str = None, icon: str = None, compiler: Compilers = Compilers.DEFAULT, onefile: bool = True, remove_output: bool = True,) -> None:
+    def __init__(self, file_path: str, output_dir: str = None, icon: str = None, compiler: Compilers = Compilers.DEFAULT, onefile: bool = True, remove_output: bool = True, window_uac_perms:bool=False) -> None:
         # file options
         self.__file = file_path
 
@@ -33,6 +33,7 @@ class ExecutableGenerator:
         # os based options
         if os_name == 'nt':
             self.__options['icon'] = icon
+            self.__options['windows-uac-admin'] = window_uac_perms
         else:
             icon = None
 
